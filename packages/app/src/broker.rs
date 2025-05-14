bc9e206ecb4000b1c7ab5a78d7b110a936eecf9f
Jorge Aparicio <jorge.aparicio@ferrous-systems.com>
2025-05-14 14:58:15 +0200

    front/settings: use preferred note name in scale tonic dropdown

--- packages/app/src/broker.rs
+++ packages/app/src/broker.rs
@@ -2,6 +2,7 @@ use crate::back::chord_namer::NoteGrouper;
 use crate::front::canvas::Canvas;
 use crate::front::console::Console;
 use crate::front::settings::midi_input::device::MidiInputDeviceSelect;
+use crate::front::settings::scale::ScaleTonicSelect;
 
 #[spur::subscriptions]
 const _: Broker = {
@@ -9,6 +10,10 @@ const _: Broker = {
     #[subscribed(to = crate::front::settings::midi_input::device::Initialize)]
     #[subscribed(to = crate::front::settings::midi_input::device::SelectChanged)]
     const _: MidiInputDeviceSelect = MidiInputDeviceSelect::new();
+
+    #[subscribed(to = crate::front::settings::scale::Initialize)]
+    #[subscribed(to = crate::messages::NewScaleTypeSelected)]
+    const _: ScaleTonicSelect = ScaleTonicSelect::new();
 
     #[subscribed(to = crate::front::canvas::Initialize)]
     #[subscribed(to = crate::messages::NewScaleTonicSelected)]
--- packages/app/src/front/settings/scale.rs
+++ packages/app/src/front/settings/scale.rs
@@ -1,7 +1,8 @@
+use alloc::vec::Vec;
 use js::{Downcast as _, Upcast as _};
-use music::{NoteName, ScaleType};
-use spur::Publish as _;
-use web::HtmlSelectElement;
+use music::{NoteName, Scale, ScaleType};
+use spur::{Message, Publish as _, React};
+use web::{HtmlOptionElement, HtmlSelectElement};
 
 use crate::broker::Broker;
 use crate::html::Form;
@@ -14,8 +15,10 @@ pub(super) fn initialize(form: &Form) {
     let tonic_select = html::select(&fieldset, &"scale-tonic".into());
     tonic_select.set_required(true);
 
+    let mut options = Vec::with_capacity(12);
     for note in NoteName::CIRCLE_OF_FIFTHS {
-        html::option(&tonic_select, None, &note.as_flat_str().into());
+        let option = html::option(&tonic_select, None, &note.as_flat_str().into());
+        options.push(option);
     }
 
     tonic_select.set_selected_index(consts::INITIAL_SCALE_TONIC_INDEX.into());
@@ -30,6 +33,45 @@ pub(super) fn initialize(form: &Form) {
 
     type_select.set_selected_index(consts::INITIAL_SCALE_TYPE_INDEX.into());
     type_select.set_onchange(|event| onchange_type(event.target().upcast().upcast().downcast()));
+
+    Broker::publish(Initialize(State { options }));
+}
+
+pub struct ScaleTonicSelect {
+    state: Option<State>,
+}
+
+impl ScaleTonicSelect {
+    pub const fn new() -> Self {
+        Self { state: None }
+    }
+}
+
+#[derive(Message)]
+pub struct Initialize(State);
+
+impl React<Initialize> for ScaleTonicSelect {
+    fn react(&mut self, Initialize(state): Initialize) {
+        self.state = Some(state);
+    }
+}
+
+impl React<NewScaleTypeSelected> for ScaleTonicSelect {
+    fn react(&mut self, NewScaleTypeSelected(index): NewScaleTypeSelected) {
+        let Some(State { options }) = &mut self.state else {
+            return;
+        };
+
+        let ty = ScaleType::ALL[index];
+
+        for (option, note) in options.iter_mut().zip(NoteName::CIRCLE_OF_FIFTHS) {
+            option.set_text_content(&js::String::from(note.as_str(Scale { tonic: note, ty })));
+        }
+    }
+}
+
+struct State {
+    options: Vec<HtmlOptionElement>,
 }
 
 fn onchange_tonic(select: HtmlSelectElement) {
