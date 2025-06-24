e935aa2cf50d7b45d7d6127624b1b257022a5cf5
Jorge Aparicio <jorge.aparicio@ferrous-systems.com>
2025-06-24 09:07:24 +0200

    format PanicInfo w/o allocations

--- packages/app/src/panic.rs
+++ packages/app/src/panic.rs
@@ -1,6 +1,7 @@
 extern crate alloc;
 
-use alloc::string::ToString;
+use core::fmt;
+use core::fmt::Write as _;
 use core::panic::PanicInfo;
 
 #[panic_handler]
@@ -10,7 +11,48 @@ fn panic(info: &PanicInfo) -> ! {
         fn ff() -> !;
     }
     if option_env!("DEBUG").is_some() {
-        web::error!(&js::String::from(info.to_string().as_str()));
+        let mut buf = WriteBuf::new();
+
+        write!(buf, "{info}").ok();
+
+        web::error!(&js::String::from(buf.as_str()));
     }
     unsafe { ff() }
+}
+
+struct WriteBuf {
+    buf: [u8; 1024],
+    cursor: usize,
+}
+
+impl WriteBuf {
+    fn new() -> Self {
+        Self {
+            buf: [0; 1024],
+            cursor: 0,
+        }
+    }
+
+    fn as_str(&self) -> &str {
+        unsafe { core::str::from_utf8_unchecked(&self.buf[..self.cursor]) }
+    }
+
+    fn capacity(&self) -> usize {
+        self.buf.len()
+    }
+
+    fn remaining(&self) -> usize {
+        self.capacity() - self.cursor
+    }
+}
+
+impl fmt::Write for WriteBuf {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        let bytes = s.as_bytes();
+        let len = bytes.len().min(self.remaining());
+        self.buf[self.cursor..][..len].copy_from_slice(&bytes[..len]);
+        self.cursor += len;
+
+        Ok(())
+    }
 }
